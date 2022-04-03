60f249e8667be8b4b37f6519be2a24fb79b6fd38
Aayushi <aayushikukreja@gmail.com>
2022-04-03 16:23:55 +0530

    Updates

---
+++ precache-manifest.46cc08f08df69c989ff80300fbb51a26.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e18ff10f0ef78fe0b67876e42d7e3efb",
+    "url": "/Keeper/index.html"
+  },
+  {
+    "revision": "e1b017533e6e40d3693f",
+    "url": "/Keeper/static/js/2.8a02f188.chunk.js"
+  },
+  {
+    "revision": "db75ce2a4950bbae849e",
+    "url": "/Keeper/static/js/main.e1275079.chunk.js"
+  },
+  {
+    "revision": "2e171cf057d5a30d7c8f",
+    "url": "/Keeper/static/js/runtime-main.cbd830f6.js"
+  }
+]);
